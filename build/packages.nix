d78cc33c5f166177556bb0bc3fa55c531cf3576a
Hans Joachim Kliemeck <h0nIg@users.noreply.github.com>
2026-01-21 09:12:53 +0100

    re-add buildInputs with root cause fix

--- build/packages.nix
+++ build/packages.nix
@@ -4,7 +4,7 @@
 }:
 let
   inherit (resolvers) resolveCyclic resolveNonCyclic;
-  inherit (lib) makeScope concatStringsSep;
+  inherit (lib) makeScope concatStringsSep unique;
 
   mkResolveBuildSystem =
     set:
@@ -69,6 +69,9 @@ let
               ++ map (pat: "--ignore-collisions ${pat}") finalAttrs.venvIgnoreCollisions
             );
           };
+
+          # expose the buildInputs - avoid "Argument list too long"
+          buildInputs = unique (pkgsFinal.resolveVirtualEnv spec);
         });
 
       hooks = pkgsFinal.callPackage ./hooks { };
